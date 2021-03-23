d96b99bb984019d77d73cf54444189a98918319f
Trevor Robinson <trevor.robi1254@gmail.com>
2021-03-23 15:53:56 -0600

    Fix user bug
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>
    Co-authored-by: Angel Breaux <48256547+abreaux26@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :user_friends, foreign_key: %i[sender_id receiver_id], class_name: 'UserFriend', dependent: :destroy, inverse_of: %i[sender receiver]
+  has_many :user_friends, foreign_key: [:sender_id, :receiver_id], class_name: 'UserFriend', dependent: :destroy
   has_many :events, class_name: 'Event', foreign_key: :host_id, dependent: :destroy, inverse_of: :host
 
   validates :username, presence: true, uniqueness: true
