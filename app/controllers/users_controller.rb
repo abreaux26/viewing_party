f493a714edf93c884a83f1fa2b1311ebe9b7f781
abreaux26 <angelbreaux26@gmail.com>
2021-03-23 20:58:17 -0500

    Create user controller and view for new
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:username] = user[:username].downcase
+    new_user = User.create(user)
+    if new_user.save
+      redirect_to dashboard_path
+    else
+      flash[:error] = new_user.errors.full_messages.to_sentence
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
