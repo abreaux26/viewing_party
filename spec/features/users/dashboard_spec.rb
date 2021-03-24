ee28d9bda5d2044e66d1ea0de62619dafc8e0433
Trevor Suter <Trevorsuter@icloud.com>
2021-03-23 18:35:17 -0600

    started on dashboard view, added users and movies controllers

---
+++ spec/features/users/dashboard_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'user dashboard page', type: :feature do
+  before :each do
+    @user1 = User.create!(username: 'user1', email: 'user1@email.com', password: 'password')
+
+    visit dashboard_path
+  end
+
+  xit 'shows a welcome header' do
+
+    expect(page).to have_content("Welcome #{@user1.username}!")
+  end
+
+  it 'has a button to discover movies' do
+
+    expect(page).to have_link('Discover Movies')
+    click_link('Discover Movies')
+    expect(current_path).to eq("/discover")
+  end
+
+  it 'has a friends section'
+
+  it 'has a viewing parties section'
+end
