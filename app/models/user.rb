0f3555fbe99a24bcf55ef79b74fb0531e6111951
Trevor Suter <Trevorsuter@icloud.com>
2021-03-23 18:34:41 -0600

    altered user and user friend models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :user_friends, foreign_key: [:sender_id, :receiver_id], class_name: 'UserFriend', dependent: :destroy
-  has_many :events, class_name: 'Event', foreign_key: :host_id, dependent: :destroy, inverse_of: :host
+  has_many :outgoing_friend_requests, foreign_key: :sender_id,
+                                      class_name: 'UserFriend',
+                                      inverse_of: :sender,
+                                      dependent: :destroy
+  has_many :incoming_friend_requests, foreign_key: :receiver_id,
+                                      class_name: 'UserFriend',
+                                      inverse_of: :receiver,
+                                      dependent: :destroy
+  has_many :events, class_name: 'Event',
+                    foreign_key: :host_id,
+                    inverse_of: :host,
+                    dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
--- app/models/user_friend.rb
+++ app/models/user_friend.rb
@@ -1,4 +1,7 @@
 class UserFriend < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+  enum status: [:pending, :accepted]
+
+  validates_presence_of :status
 end
