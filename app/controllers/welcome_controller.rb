64f21f0c0bf7be18d21bff8362fc755d125277f5
abreaux26 <angelbreaux26@gmail.com>
2021-03-23 20:57:04 -0500

    Create welcome controller and welcome view
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    binding.pry
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Invalid Credentials!"
+      render :index
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
