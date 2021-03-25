7c78b9c2580c3ed1203e36a09af14b88c13c0eab
Trevor Robinson <trevor.robi1254@gmail.com>
2021-03-25 12:01:23 -0600

    Create Movie Service file
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>
    Co-authored-by: Angel Breaux <48256547+abreaux26@users.noreply.github.com>

---
+++ app/services/movie_service.rb
@@ -0,0 +1,26 @@
+require 'pry'
+require 'faraday'
+require 'json'
+
+class MovieService
+
+  def top_40
+    get_data('/3/movie/top_rated')
+  end
+
+  def get_data(url)
+   response = authorize.get(url)
+   data = response.body
+   parse = JSON.parse(data, symbolize_names: true)
+   binding.pry
+ end
+
+ def authorize
+   Faraday.new('https://api.themoviedb.org') do |conn|
+     conn.authorization :Bearer, 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTAyZGZjNzViMzBkZDkwYTZhYTNmNWMxYThmMjBhMiIsInN1YiI6IjYwNWJlYzI0OTQ0YTU3MDAyOWE2NWFhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BESdLe_XE56rDoqKxvLa1kE6XwqEdxmgjNBJsgwGc_s'
+     # conn.headers('Authorization: Bearer, eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTAyZGZjNzViMzBkZDkwYTZhYTNmNWMxYThmMjBhMiIsInN1YiI6IjYwNWJlYzI0OTQ0YTU3MDAyOWE2NWFhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BESdLe_XE56rDoqKxvLa1kE6XwqEdxmgjNBJsgwGc_s')
+   end
+ end
+end
+m = MovieService.new
+m.top_40
