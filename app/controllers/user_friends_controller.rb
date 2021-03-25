d9c6d403875e56d4ebb57cffdc362a86df14660e
abreaux26 <angelbreaux26@gmail.com>
2021-03-24 20:48:11 -0500

    Add new user friends controller
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>

---
+++ app/controllers/user_friends_controller.rb
@@ -0,0 +1,13 @@
+class UserFriendsController < ApplicationController
+  def create
+    if params[:friend]
+      friend = User.find_by_email(params[:friend])
+      if friend.nil?
+        flash[:error] = "User doesn't exist."
+      else
+        current_user.user_friends.create!(friend: friend)
+      end
+    end
+    redirect_to dashboard_path
+  end
+end
