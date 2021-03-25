e2265a4f075282eab76fc3704c95986e5f992472
Trevor Suter <Trevorsuter@icloud.com>
2021-03-25 13:59:46 -0600

    api bearer token in progress
    Co-authored-by: Angel Breaux <48256547+abreaux26@users.noreply.github.com>
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -5,21 +5,22 @@ require 'json'
 class MovieService
 
   def top_40
-    get_data('/3/movie/top_rated')
+    get_url('https://api.themoviedb.org/3/movie/top_rated')
   end
 
   def get_data(url)
    response = authorize.get(url)
    data = response.body
-   parse = JSON.parse(data, symbolize_names: true)
-   binding.pry
- end
+   JSON.parse(data, symbolize_names: true)
+  end
 
- def authorize
-   Faraday.new('https://api.themoviedb.org') do |conn|
-     conn.authorization :Bearer, 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxNTAyZGZjNzViMzBkZDkwYTZhYTNmNWMxYThmMjBhMiIsInN1YiI6IjYwNWJlYzI0OTQ0YTU3MDAyOWE2NWFhMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BESdLe_XE56rDoqKxvLa1kE6XwqEdxmgjNBJsgwGc_s'
+ def get_url(url)
+   Faraday.get(url) do |conn|
+     conn.headers['Authorization'] = "Bearer #{ENV['MOVIEDB_TOKEN']}"
    end
  end
 end
 m = MovieService.new
-m.top_40
+binding.pry
+
+p m.top_40
