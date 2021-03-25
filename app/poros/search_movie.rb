71cea07db7de6ce981d21352476a63b7aed53030
Trevor Suter <Trevorsuter@icloud.com>
2021-03-25 17:53:43 -0600

    top movies working, not tested

---
+++ app/poros/search_movie.rb
@@ -0,0 +1,11 @@
+class SearchMovie
+  attr_reader :title,
+              :vote_average,
+              :id
+  
+  def initialize(data)
+    @id = data[:id]
+    @title = data[:title]
+    @vote_average = data[:vote_average]
+  end
+end
--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,22 +1,22 @@
-require 'pry'
-require 'faraday'
-require 'json'
-
 class MovieService
+  
+  def connection
+    Faraday.new("https://api.themoviedb.org") do |conn|
+      conn.authorization :Bearer, "#{ENV['MOVIEDB_TOKEN']}"
+    end
+  end
 
   def top_40
-    get_url('https://api.themoviedb.org/3/movie/top_rated')
+    page1 = get_data('/3/movie/top_rated?page=1')
+    page2 = get_data('/3/movie/top_rated?page=2')
+    (page1[:results] + page2[:results]).map do |data|
+      SearchMovie.new(data)
+    end
   end
 
   def get_data(url)
-    response = authorize.get(url)
+    response = connection.get(url)
     data = response.body
     JSON.parse(data, symbolize_names: true)
-  end
-
-  def get_url(url)
-    Faraday.get(url) do |conn|
-      conn.headers['Authorization'] = "Bearer #{ENV['MOVIEDB_TOKEN']}"
-    end
   end
 end
