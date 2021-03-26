98ebdd9bfce141244a5a87b210b185e7f7a7242d
abreaux26 <angelbreaux26@gmail.com>
2021-03-25 20:18:16 -0500

    Instantiate new movie service
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :movie_service
+
+  def movie_service
+    @movie_service ||= MovieService.new
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
