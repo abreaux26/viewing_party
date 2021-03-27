3e4b019c412763f2f0ee2acbffacf2ec8ee21877
abreaux26 <angelbreaux26@gmail.com>
2021-03-27 00:24:33 -0500

    Fix rubocop

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -12,7 +12,7 @@ class MovieService
   end
 
   def movie_detail_for(movie_id)
-    @_movie_detail_for ||= get_data("/3/movie/#{movie_id}")
+    @movie_detail_for ||= get_data("/3/movie/#{movie_id}")
   end
 
   def genres(movie_id)
