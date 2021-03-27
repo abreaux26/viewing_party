a94faada49cf13e62771ce8f1f9e2516aa3c6adc
Trevor Suter <Trevorsuter@icloud.com>
2021-03-26 19:13:44 -0600

    rubocop errors fixed
    Co-authored-by: Angel Breaux <48256547+abreaux26@users.noreply.github.com>
    Co-authored-by: Trevor Robinson <72584659+Trevor-Robinson@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,12 +2,10 @@ class MoviesController < ApplicationController
   def discover; end
 
   def search
-    if params[:q] == "top_rated"
+    if params[:q] == 'top_rated'
       @top_forty = @movie_service.top_forty
     elsif params[:q]
       @search = @movie_service.search(params[:q])
-    else
     end
-
   end
 end
