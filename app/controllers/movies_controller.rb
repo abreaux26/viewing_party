5eedeef2557efe98d83adb766d8211e72913dfe3
Trevor Robinson <trevor.robi1254@gmail.com>
2021-03-26 18:26:51 -0600

    Run rubocop
    Co-authored-by: Trevor Suter <72848529+Trevorsuter@users.noreply.github.com>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,7 @@
 class MoviesController < ApplicationController
-  def discover
-  end
+  def discover; end
 
   def search
-    @top_forty = @movie_service.top_forty if params[:q] == "top_rated"
+    @top_forty = @movie_service.top_forty if params[:q] == 'top_rated'
   end
 end
