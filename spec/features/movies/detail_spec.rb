336da1ec018850cc5682dcd09c8ee25959030b00
abreaux26 <angelbreaux26@gmail.com>
2021-03-27 00:39:02 -0500

    Finish movie detail spec

--- spec/features/movies/detail_spec.rb
+++ spec/features/movies/detail_spec.rb
@@ -1,24 +1,30 @@
 require 'rails_helper'
 RSpec.describe "When I visit the movies detail page", type: :feature do
-  xit 'I see information about the movie' do
+  before :each do
+    @user1 = User.create!(username: 'user1', email: 'user1@email.com', password: 'password')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+  end
+
+  it 'I see information about the movie' do
     visit discover_path
 
-    VCR.use_cassette('top_forty') do
-      click_on('Find Top 40 Movies')
-      # binding.pry
-      # find(".title", match: :first)
-      # find('.movie_id', match: :first, visible: false).text
-      # find(".movie_id", match: :first).text
+    VCR.use_cassette('movie_data_details') do
+      fill_in :q, with: 'Phoenix'
+      click_on('Search by Movie Title')
+
+      movie_title = find(".title", match: :first).text
+      vote_average = find(".vote_average", match: :first).text
+      click_on(movie_title)
 
       # expect(current_path).to eq(movie_path(first(".movie").id))
 
-      # expect(page).to have_css(".title")
-      # expect(page).to have_css(".vote_average")
+      expect(page).to have_content(movie_title)
+      expect(page).to have_content(vote_average)
       # expect(page).to have_css(".runtime")
       # expect(page).to have_css(".genres")
       expect(page).to have_css(".summary")
-      expect(page).to have_css(".cast")
-      expect(page).to have_css(".review")
+      expect(page).to have_css(".casts")
+      expect(page).to have_css(".reviews")
     end
   end
 end
