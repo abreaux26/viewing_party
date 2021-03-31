c8e781ca1834df334321f64b3b45b46e7ca02cba
Trevor Robinson <trevor.robi1254@gmail.com>
2021-03-31 16:21:59 -0600

    Heroku Email Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "viewing_party_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = { domain: 'https://powerful-cove-00403.herokuapp.com/', address: "smtp.sendgrid.net", port: 587, authentication: :plain, user_name: 'apikey', password: ENV['SENDGRID_API_KEY'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -95,6 +98,5 @@ Rails.application.configure do
   #images will deploy to heroku
   config.serve_static_assets = true
   config.assets.compile = true
-  config.action_mailer.delivery_method = :smtp config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = { domain: 'https://powerful-cove-00403.herokuapp.com/', address: "smtp.sendgrid.net", port: 587, authentication: :plain, user_name: 'apikey', password: ENV['SENDGRID_API_KEY'] }
+
 end
